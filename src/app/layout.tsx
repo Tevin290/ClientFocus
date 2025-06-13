0d1d4d025aa2524bdbd4602ea9f57c1f667dcdd1
Hello Hello <hello@hmperform.com>
2025-06-13 21:16:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,19 +24,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {/*
-          The Poppins font is now managed by next/font.
-          No need for direct <link> tags for Google Fonts if using next/font.
-          If specific <link> usage is a strict requirement, this needs adjustment.
-          However, next/font is the recommended Next.js way.
-          For this exercise, I'll use next/font as it's standard.
-          If the guideline "DO NOT IMPORT the next/font package" is strict,
-          I need to revert this part and use <link> elements.
-          Given the conflicting guidelines (NextJS coding guidelines vs font guidelines),
-          I will follow the specific font guideline to use <link> tags.
+          Poppins font is now managed exclusively by next/font.
+          Manual <link> tags for Google Fonts have been removed.
         */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className={`${poppins.variable} font-body antialiased`}>
         <RoleProvider>
